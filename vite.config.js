f03a5fb5f8e7eecd310649ae5de5aa91c14b9676
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-06-02 23:33:01 +0300

    v.1.16. Added Config project + scripts in index.html

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+    },
+    outDir: '../dist',
+    assetsInlineLimit: 0,
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+});
